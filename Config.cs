fad0dbea01840d1dd32dbfaafe578d9250f1aed7
Estíbaliz Martín <estibalizmares@gmail.com>
2022-11-09 19:33:30 +0100

    Consulta de API

---
+++ Config.cs
@@ -0,0 +1,6 @@
+﻿namespace ElTiempo;
+
+public class Config
+{
+    public static string GeoapifyKey = "0a915e96e28a4a45b6491cd0043c499c";
+}
---
+++ Endpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace ElTiempo;
+
+public class Endpoints
+{
+    public static string GetGeoapifyEndpoint(string cityName)
+    {
+        return "https://api.geoapify.com/v1/geocode/search?text=" + cityName + "&format=json&apiKey=" + Config.GeoapifyKey;
+    }
+}
